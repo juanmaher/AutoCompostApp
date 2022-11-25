94e9ff268dedcb5d5e313ca49c707a0065e6bc1b
juanmaher <jmhernandez1440@gmail.com>
2022-11-24 21:03:48 -0300

    Agrego lectura de datos a firebase
    Se agrego la lectura y escritura a firebase. Se puede ver los valores de temperatura, humedad y que tan completa está la compostera. Además, se agregan los comandos para activar la trituradora y la mezcladora.

--- lib/app/ui/pages/my_autocompost/my_autocompost_page.dart
+++ lib/app/ui/pages/my_autocompost/my_autocompost_page.dart
@@ -1,16 +1,21 @@
 import 'package:autocompost/app/ui/routes/routes.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_meedu/ui.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 
-class MyAutoCompostPage extends StatelessWidget {
+class MyAutoCompostPage extends StatefulWidget {
   const MyAutoCompostPage({Key? key}) : super(key: key);
 
   @override
+  State<MyAutoCompostPage> createState() => _MyAutoCompostPageState();
+}
+
+class _MyAutoCompostPageState extends State<MyAutoCompostPage> {
+
+  @override
   Widget build(BuildContext context) {
-    // Obtengo el alto y el ancho de la pantalla
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text('AutoCompost'),
@@ -21,21 +26,115 @@ class MyAutoCompostPage extends StatelessWidget {
           },
         ),
       ),
-      body: Column(
+      body: const MyAutoCompostPageBody(),
+    );
+  }
+}
+
+class MyAutoCompostPageBody extends StatefulWidget {
+  const MyAutoCompostPageBody({Key? key}) : super(key: key);
+
+  @override
+  State<MyAutoCompostPageBody> createState() => _MyAutoCompostPageBodyState();
+}
+
+class _MyAutoCompostPageBodyState extends State<MyAutoCompostPageBody> {
+
+  final _database = FirebaseDatabase.instance.ref();
+
+  bool isLoading = false;
+
+  late int _complete = 0;
+  late int _temperature = 0;
+  late int _humidity = 0;
+  late bool _mezcladora = false;
+  late bool _trituradora = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _activateListeners();
+  }
+
+  void _activateListeners(){
+    _database.child('/composters/1/complete').onValue.listen((event) {
+      final int data1 = (event.snapshot.value ?? 0) as int;
+      setState(() {
+        _complete = data1;
+      });
+    });
+    _database.child('/composters/1/temperature').onValue.listen((event) {
+      final int data2 = (event.snapshot.value ?? 0) as int;
+      setState(() {
+        _temperature = data2;
+      });
+    });
+    _database.child('/composters/1/humidity').onValue.listen((event) {
+      final int data3 = (event.snapshot.value ?? 0) as int;
+      setState(() {
+        _humidity = data3;
+      });
+    });
+    _database.child('/composters/1/mezcladora').onValue.listen((event) {
+      final bool data4 = (event.snapshot.value ?? false) as bool;
+      setState(() {
+        _mezcladora = data4;
+      });
+    });
+    _database.child('/composters/1/trituradora').onValue.listen((event) {
+      final bool data5 = (event.snapshot.value ?? false) as bool;
+      setState(() {
+        _trituradora = data5;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    final _databaseComposterRef = _database.child('/composters/1');
+
+    // Obtengo el alto y el ancho de la pantalla
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
         children: <Widget>[
-          SizedBox(
+          Container(
+            margin: const EdgeInsets.only(
+              bottom: 20,
+            ),
             // Solo tomo el 20% del alto de la pantalla
             height: size.height * 0.2,
             child: Stack(
               children: <Widget>[
                 Container(
-                  height: size.height*0.2 - 27,
+                  padding: const EdgeInsets.only(
+                    left: 30,
+                    right: 20,
+                    bottom: 20,
+                  ),
+                  height: size.height * 0.2 - 27,
                   decoration: const BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(36),
-                      bottomRight: Radius.circular(36),
-                    )
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(36),
+                        bottomRight: Radius.circular(36),
+                      )
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        'Mi Compostera',
+                        style: Theme
+                            .of(context)
+                            .textTheme
+                            .headline5
+                            ?.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
                 Positioned(
@@ -44,7 +143,7 @@ class MyAutoCompostPage extends StatelessWidget {
                   right: 0,
                   child: Container(
                     margin: const EdgeInsets.symmetric(horizontal: 20),
-                    height: 54,
+                    height: 60,
                     decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(20),
@@ -61,8 +160,8 @@ class MyAutoCompostPage extends StatelessWidget {
                       animation: true,
                       lineHeight: 20.0,
                       animationDuration: 2500,
-                      percent: 0.8,
-                      //center: const Text("80.0%"),
+                      percent: _complete.toDouble() / 100,
+                      center: Text('$_complete%'),
                       barRadius: const Radius.circular(36),
                       progressColor: Colors.green,
                     ),
@@ -70,9 +169,144 @@ class MyAutoCompostPage extends StatelessWidget {
                 ),
               ],
             ),
-          )
+          ),
+          Column(
+            children: [
+              SizedBox(
+                height: 100,
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 20),
+                      padding: const EdgeInsets.only(
+                        left: 30,
+                        right: 20,
+                        top: 20,
+                        bottom: 20,
+                      ),
+                      height: size.height * 0.2 - 27,
+                      decoration: const BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          topRight: Radius.circular(12),
+                          bottomLeft: Radius.circular(12),
+                          bottomRight: Radius.circular(12),
+                        ),
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            'Temperatura: $_temperature',
+                            style: Theme.of(context).textTheme.headline6?.copyWith(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                height: 100,
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 20),
+                      padding: const EdgeInsets.only(
+                        left: 30,
+                        right: 20,
+                        top: 20,
+                        bottom: 20,
+                      ),
+                      height: size.height * 0.2 - 27,
+                      decoration: const BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          topRight: Radius.circular(12),
+                          bottomLeft: Radius.circular(12),
+                          bottomRight: Radius.circular(12),
+                        ),
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            'Humedad: $_humidity',
+                            style: Theme.of(context).textTheme.headline6?.copyWith(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+                        (Set<MaterialState> states) {
+                      if (_trituradora != true) {
+                        return Colors.grey;
+                      }
+                      return Colors.green;
+                    },
+                  ),
+                ),
+                onPressed: () {
+                  if(_trituradora != true) {
+                    _databaseComposterRef.update({'trituradora': true});
+                  } else {
+                    _databaseComposterRef.update({'trituradora': false});
+                  }
+                },
+                child: Text(
+                  'Trituradora',
+                  style: Theme.of(context).textTheme.headline6?.copyWith(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+                        (Set<MaterialState> states) {
+                      if (_mezcladora != true) {
+                        return Colors.grey;
+                      }
+                      return Colors.green;
+                    },
+                  ),
+                ),
+                onPressed: () {
+                  if(_mezcladora != true) {
+                    _databaseComposterRef.update({'mezcladora': true});
+                  } else {
+                    _databaseComposterRef.update({'mezcladora': false});
+                  }
+                },
+                child: Text(
+                  'Mezcladora',
+                  style: Theme.of(context).textTheme.headline6?.copyWith(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
         ],
       ),
     );
   }
 }
+
