1e3f75867be610e56165b0aae1f5e1c4b5265d78
juanmaher <jmhernandez1440@gmail.com>
2022-12-02 16:17:08 -0300

    Desactivo orientación landscape

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:autocompost/app/autocompost.dart';
 import 'package:autocompost/app/inject_dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -9,7 +10,10 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  injectDependencies();
-  runApp(const AutoCompostApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_){
+    injectDependencies();
+    runApp(const AutoCompostApp());
+  });
 }
 
