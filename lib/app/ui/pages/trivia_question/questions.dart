4c57fa202fd2216fce02cbcba770f75c465c73a9
aguStrus <astrusberg@fi.uba.ar>
2022-12-04 17:51:37 -0300

    Orden preguntas random

--- lib/app/ui/pages/trivia_question/questions.dart
+++ lib/app/ui/pages/trivia_question/questions.dart
@@ -5,23 +5,20 @@ import 'question_model.dart';
 
 class QuizBrain {
   int _questionNum = 0;
-  List<int> _randomOrder = [];
   final List<Question> _questionBank = [
-    Question('¿Qué tipo de lombrices son más adecuadas para una compostera?', ["Lombrices de tierra epigeas", "Lombrices rojas californianas", "Lombrices africanas", "Otras lombrices"], 1),
+    Question('¿Qué tipo de lombrices son más adecuadas para una compostera?', ["Lombrices de tierra epigeas", "Lombrices rojas californianas", "Lombrices de tierra endógeas"], 1),
     Question('¿Cuales son las 3 "R" de la ecología?', ["Reciclar, Reutilizar, Reducir", "Reutilizar, Recuperar, Reservar"], 0),
-    Question('¿Cuanto dura el proceso de compost?', ["De 3 a 6 meses", "De 7 a 9 meses"], 0),
+    Question('¿Cuanto dura el proceso de compost?', ["De 3 a 6 meses", "De 7 a 9 meses", "De 10 a 12 meses"], 0),
     Question('En el proceso de compostaje los materiales orgánicos se degradan de forma:', ["Química, por la reacción con el nitrógeno del aire", "Biológica, a través de microorganismos"], 1),
-    Question('Si el material degradado presenta mal olor o aparecen mosquitas, debo:', ["Adicionar material estructurante y voltear la mezcla", "Regar"], 0),
+    Question('Si el material degradado presenta mal olor o aparecen mosquitas, debo:', ["Adicionar material estructurante y voltear la mezcla", "Regar", "Dejar de agregar material por un tiempo"], 0),
     Question('¿Qué materiales NO se pueden poner en una compostera domiciliaria?',
-        ["Yerba y café", "Lácteos y aceites"], 1),
+        ["Yerba y café", "Papeles y cartones","Lácteos y aceites"], 2),
     Question('El compostaje es un proceso:', ["Anaeróbico (que no requiere oxígeno)", "Aeróbico (que requiere oxígeno)"], 1),
-    Question('Aproximadamente: ¿Que porcentaje de nuestra basura es organica en Argentina?', ["49%", "33%"], 0)
+    Question('Aproximadamente: ¿Que porcentaje de nuestra basura es organica en Argentina?', ["58%","49%", "33%"], 1),
+    Question('¿Cuál es la proporción recomendada para diluir el liquido lixidiado?', ["1 en 2","1 en 5", "1 en 10"], 2)
   ];
 
   QuizBrain(){
-    for(int i = 0; i < _questionBank.length; i++) {
-      _randomOrder.add(i);
-    }
     _questionBank.shuffle();
   }
 
